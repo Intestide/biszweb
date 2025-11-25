f8551cb5c83c2fd110f17f8f471eb68ad6354dc6
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-11-25 18:32:54 +0800

    added font preload

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,36 @@
-export default{
-    build:{
-        sourcemap: true,
-    }
-}
+import {defineConfig} from "vite";
+import fs from "fs";
+import path from "path";
+export default defineConfig({
+    plugins:[
+        {
+            name: "preload-ttf-fonts",
+            transformIndexHtml(){
+                let fontsDir=path.resolve(__dirname, "public/fonts");
+                let preloadTags=[];
+                if (fs.existsSync(fontsDir)){
+                    let files=fs.readdirSync(fontsDir);
+                    let targetFonts=["EBGaramond-VariableFont_wght.ttf", "NotoSansSC-VariableFont_wght.ttf"];
+                    targetFonts.forEach(font=>{
+                        if (files.includes(font)){
+                            preloadTags.push({
+                                tag: "link",
+                                attrs: {
+                                    rel: "preload",
+                                    as: "font",
+                                    href: `/fonts/${font}`,
+                                    type: "font/ttf",
+                                    crossorigin: ""
+                                },
+                                injectTo: "head"
+                            });
+                        }
+                    });
+                }
+                return{
+                    tags: preloadTags
+                };
+            }
+        }
+    ]
+});
